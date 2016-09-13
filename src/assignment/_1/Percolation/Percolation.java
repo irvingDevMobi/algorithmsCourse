93d44221a07f2312e78b6e1d195745a50242b1aa
Irving Lóp <irving.lopez@unam.mobi>
2016-09-12 22:13:16 -0500

    [Assignment 1] algs4 library and Percolation class added

---
+++ src/assignment/_1/Percolation/Percolation.java
@@ -0,0 +1,16 @@
+package assignment._1.Percolation;
+
+/**
+ * Created by Irving Lop on 12/09/2016.
+ *
+ */
+public class Percolation {
+
+
+    /**
+     * create n-by-n grid, with all sites blocked
+     * @param n size of the square grid
+     */
+    public Percolation(int n) {
+    }
+}
