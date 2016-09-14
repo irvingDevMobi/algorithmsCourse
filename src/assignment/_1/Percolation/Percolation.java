3a718dbe908b45dd4e9c576925ddae239c7db2b1
Irving Lóp <irving.lopez@unam.mobi>
2016-09-13 22:39:07 -0500

    [Assignment 1] Constructor added - little advance

--- src/assignment/_1/Percolation/Percolation.java
+++ src/assignment/_1/Percolation/Percolation.java
@@ -1,16 +1,31 @@
 package assignment._1.Percolation;
 
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 /**
  * Created by Irving Lop on 12/09/2016.
  *
  */
 public class Percolation {
 
+    private int [][] grid;
+    private WeightedQuickUnionUF wQUUF;
 
     /**
      * create n-by-n grid, with all sites blocked
      * @param n size of the square grid
      */
     public Percolation(int n) {
+        grid = new int[n][n];
+        int index = 1;
+        for (int row = 0; row < n; row++)
+        {
+            for (int column = 0; column < n; column++)
+            {
+                grid[row][column] = index;
+                index++;
+            }
+        }
+        wQUUF = new WeightedQuickUnionUF(n*n+2);
     }
 }
