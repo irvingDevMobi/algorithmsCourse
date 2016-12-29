135fb4232f061540b24b1aa6c4ff3942f1ef3ff7
Irving Lóp <irving.lopez@unam.mobi>
2016-12-28 21:34:25 -0600

    until exercise 2.11

--- Weiss/_2/Practice.java
+++ Weiss/_2/Practice.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+
 public class Practice {
 	
 	public static void main(String ... args) 
@@ -14,6 +18,22 @@ public class Practice {
 		} else {
 			System.out.println(strings[post] + " is NOT postfix of " + strings[word]);
 		}
+		
+		int [] five = { 1, 2, 3, 4, 5};
+		System.out.println("Size:" + five.length);
+		five = resize(five);
+		System.out.println("Size:" + five.length);
+		
+		if (areEquals(args[0], args[1]))
+		{
+			System.out.println("They are same");
+		} else
+		{
+			System.out.println("They are different");
+		}
+		double [] array = { 6, 8, 8, 9, 7, 10, 8};
+		System.out.println("Sum: \t" + sum(array));
+		System.out.println("Average: \t" + average(array));
 	}
 	
 	public static boolean isPostfixOf(String str1, String str2)
@@ -31,5 +51,57 @@ public class Practice {
 			return true;
 		}
 		return false;
+	}
+	
+	public static int [] resize(int [] array) 
+	{
+		int [] newArray = new int[array.length * 2 + 1];
+		for (int i = 0; i < array.length; i++)
+		{
+			newArray[i] = array[i];
+		}
+		return newArray;
+	}
+
+	public static boolean areEquals(String file1, String file2)	
+	{
+		try {
+			return sumCharactersFile(file1) == sumCharactersFile(file2);
+		} catch(FileNotFoundException e)
+		{
+			System.err.println(e.getMessage());
+			return false;
+		}
+	}
+	
+	private static int sumCharactersFile(String pathFile) throws FileNotFoundException
+	{
+		Scanner scanner = new Scanner(new FileReader(pathFile));
+		int sum = 0;
+		while(scanner.hasNextLine())
+		{
+			String line = scanner.nextLine(); 
+			for (int c = 0; c < line.length(); c++)
+			{
+				sum += line.charAt(c);
+			}
+		}
+		System.out.println(sum);
+		return sum;
+	}
+	
+	public static double sum(double [] array)
+	{
+		double sum = 0;
+		for(double d : array)
+		{
+			sum +=  d;
+		}
+		return sum;
+	}
+	
+	public static double average(double [] array)
+	{
+		return sum(array)/array.length;
 	}
 }
