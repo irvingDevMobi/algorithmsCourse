fba2fed310d5a2311f98b2c77953408c2ccdbb6e
Irving Lopez <ilopezp@gbm.com.mx>
2019-02-08 10:54:14 -0600

    [Python Course] Array Pair problem - course solution

--- UdemyCoursePy/src/mx/irving/arryas/PairSum.py
+++ UdemyCoursePy/src/mx/irving/arryas/PairSum.py
@@ -1,11 +1,11 @@
 import unittest
 
 
-def get_pair_sum(list, objective):
+def get_pair_sum(my_list, objective):
     count = 0
-    for i in range(len(list) - 1):
-        for j in range(i + 1, len(list)):
-            if list[i] + list[j] == objective:
+    for i in range(len(my_list) - 1):
+        for j in range(i + 1, len(my_list)):
+            if my_list[i] + my_list[j] == objective:
                 count += 1
     return count
 
@@ -16,13 +16,31 @@ print("pairs: ", test1)
 print("pairs: ", test2)
 
 
+def get_pair_sum_sol(my_list, objective):
+    if len(my_list) < 2:
+        return 0
+    seen = set()
+    output = set()
+
+    for number in my_list:
+        difference_target = objective - number
+
+        if difference_target not in seen:
+            seen.add(number)
+        else:
+            output.add((min(number, difference_target), max(number, difference_target)))
+
+    return len(output)
+
+
 class TestPair(unittest.TestCase):
 
     def test(self, sol):
+        self.assertEqual(sol([1], 1), 0)
         self.assertEqual(sol([1, 9, 2, 8, 3, 7, 4, 6, 5, 5, 13, 14, 11, 13, -1], 10), 6)
-        #self.assertEqual(sol([1, 2, 3, 1], 3), 1)
+        self.assertEqual(sol([1, 2, 3, 1], 3), 1)
         self.assertEqual(sol([1, 3, 2, 2], 4), 2)
 
 
 t = TestPair()
-t.test(get_pair_sum)
+t.test(get_pair_sum_sol)
