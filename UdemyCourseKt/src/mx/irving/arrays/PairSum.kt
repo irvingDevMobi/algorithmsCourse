b89945bde4f481516d447e8dc9d74bf8af68ba00
Irving Lopez <ilopezp@gbm.com.mx>
2019-02-12 09:12:37 -0600

    [Kotlin Project] Arrays: Pair Sum - course solution

--- UdemyCourseKt/src/mx/irving/arrays/PairSum.kt
+++ UdemyCourseKt/src/mx/irving/arrays/PairSum.kt
@@ -1,5 +1,8 @@
 package mx.irving.arrays
 
+import kotlin.math.max
+import kotlin.math.min
+
 fun pairSum(list: List<Int>, objective: Int): Int {
     var count = 0
     for (x in list.indices) {
@@ -9,5 +12,17 @@ fun pairSum(list: List<Int>, objective: Int): Int {
         }
     }
     return count
+}
+
+fun pairSumSol(list: List<Int>, objective: Int): Int {
+    if (list.size < 2) return 0
+    val seen = HashSet<Int>()
+    val output = HashSet<Pair<Int, Int>>()
+    for (num in list) {
+        val differenceTarget = objective - num
+        if (differenceTarget in seen) output.add(Pair(min(num, differenceTarget), max(num, differenceTarget)))
+        else seen.add(num)
+    }
+    return output.size
 }
 
--- UdemyCourseKt/src/test/arrays/PairSumTest.kt
+++ UdemyCourseKt/src/test/arrays/PairSumTest.kt
@@ -1,6 +1,7 @@
 package test.arrays
 
 import mx.irving.arrays.pairSum
+import mx.irving.arrays.pairSumSol
 import org.junit.Assert.assertEquals
 import org.junit.Test
 
@@ -8,7 +9,18 @@ class PairSumTest {
 
     @Test
     fun test() {
+        assertEquals(0, pairSum(listOf(1), 5))
+        assertEquals(0, pairSum(listOf(1, 5), 5))
         assertEquals(6, pairSum(listOf(1, 9, 2, 8, 3, 7, 4, 6, 5, 5, 13, 14, 11, 13, -1), 10))
         assertEquals(2, pairSum(listOf(1, 3, 2, 2), 4))
+    }
+
+    @Test
+    fun testSol() {
+        assertEquals(0, pairSumSol(listOf(1), 5))
+        assertEquals(0, pairSumSol(listOf(1, 5), 5))
+        assertEquals(6, pairSumSol(listOf(1, 9, 2, 8, 3, 7, 4, 6, 5, 5, 13, 14, 11, 13, -1), 10))
+        assertEquals(2, pairSumSol(listOf(1, 3, 2, 2), 4))
+        assertEquals(1, pairSumSol(listOf(1, 3, 2, 1), 3))
     }
 }
