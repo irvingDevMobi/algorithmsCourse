eb21687f6946a36ecf4e6566de06d6aa00ec33dc
Irving Lopez <ilopezp@gbm.com.mx>
2019-02-18 09:22:51 -0600

    [Python Course] Find missing element - course solutions

--- UdemyCoursePy/src/mx/irving/arryas/FindMissingElement.py
+++ UdemyCoursePy/src/mx/irving/arryas/FindMissingElement.py
@@ -1,4 +1,5 @@
 import unittest
+import collections
 
 
 def find(array1, array2):
@@ -6,6 +7,43 @@ def find(array1, array2):
     for index in range(len(array2)):
         difference += array1[index] - array2[index]
     return difference + array1[len(array1) - 1]
+
+
+def finder(arr1, arr2):
+    arr1.sort()
+    arr2.sort()
+
+    for num1, num2 in zip(arr1, arr2):
+        if num1 != num2:
+            return num1
+
+    return arr1[-1]
+
+
+def finder2(arr1, arr2):
+    d = collections.defaultdict(int)
+
+    # Add a count for every instance in Array 2
+    for num in arr2:
+        d[num] += 1
+
+    # Check if num not in dictionary
+    for num in arr1:
+        if d[num] == 0:
+            return num
+
+        else:
+            d[num] -= 1
+
+
+def finder3(arr1, arr2):
+    result = 0
+
+    # Perform an XOR between the numbers in the arrays
+    for num in arr1 + arr2:
+        result ^= num
+
+    return result
 
 
 class TestFinder(unittest.TestCase):
@@ -19,3 +57,6 @@ class TestFinder(unittest.TestCase):
 
 t = TestFinder()
 t.test(find)
+t.test(finder)
+t.test(finder2)
+t.test(finder3)
