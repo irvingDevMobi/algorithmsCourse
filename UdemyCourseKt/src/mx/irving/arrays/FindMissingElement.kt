7aaea09d7b1c2a79d9130116a8665b6989e3b777
Irving Lopez <ilopezp@gbm.com.mx>
2019-02-21 08:58:40 -0600

    [Kotlin Project] Arrays: Find Missing element - course solutions

--- UdemyCourseKt/src/mx/irving/arrays/FindMissingElement.kt
+++ UdemyCourseKt/src/mx/irving/arrays/FindMissingElement.kt
@@ -7,3 +7,38 @@ fun find(array1: Array<Int>, array2: Array<Int>): Int {
     }
     return difference + array1.last()
 }
+
+fun finder(arr1: Array<Int>, arr2: Array<Int>): Int {
+    arr1.sort()
+    arr2.sort()
+
+    for ((num1, num2) in arr1.zip(arr2)) {
+        if (num1 != num2)
+            return num1
+    }
+    return arr1.last()
+}
+
+fun finder2(array1: Array<Int>, array2: Array<Int>): Int {
+    val map = hashMapOf<Int, Int>()
+
+    for (num in array2) {
+        map[num] = map[num]?.plus(1) ?: 1
+    }
+
+    for (num in array1) {
+        if (map[num] == 0 || !map.containsKey(num))
+            return num
+        else map[num] = map[num]?.minus(1) ?: -1
+    }
+    return -1
+}
+
+fun finder3(array1: Array<Int>, array2: Array<Int>): Int {
+    var result = 0
+    // Perform an XOR operation between the numbers in the arrays
+    for (num in array1 + array2) {
+        result = result xor num
+    }
+    return result
+}
--- UdemyCourseKt/src/test/arrays/FindMissingElementTest.kt
+++ UdemyCourseKt/src/test/arrays/FindMissingElementTest.kt
@@ -1,6 +1,9 @@
 package test.arrays
 
 import mx.irving.arrays.find
+import mx.irving.arrays.finder
+import mx.irving.arrays.finder2
+import mx.irving.arrays.finder3
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -11,5 +14,26 @@ class FindMissingElementTest {
         assertEquals(5, find(arrayOf(5, 5, 7, 7), arrayOf(5, 7, 7)))
         assertEquals(5, find(arrayOf(1, 2, 3, 4, 5, 6, 7), arrayOf(3, 7, 2, 1, 4, 6)))
         assertEquals(6, find(arrayOf(9, 8, 7, 6, 5, 4, 3, 2, 1), arrayOf(9, 8, 7, 5, 4, 3, 2, 1)))
+    }
+
+    @Test
+    fun testFinder() {
+        assertEquals(5, finder(arrayOf(5, 5, 7, 7), arrayOf(5, 7, 7)))
+        assertEquals(5, finder(arrayOf(1, 2, 3, 4, 5, 6, 7), arrayOf(3, 7, 2, 1, 4, 6)))
+        assertEquals(6, finder(arrayOf(9, 8, 7, 6, 5, 4, 3, 2, 1), arrayOf(9, 8, 7, 5, 4, 3, 2, 1)))
+    }
+
+    @Test
+    fun testFinder2() {
+        assertEquals(5, finder2(arrayOf(5, 5, 7, 7), arrayOf(5, 7, 7)))
+        assertEquals(5, finder2(arrayOf(1, 2, 3, 4, 5, 6, 7), arrayOf(3, 7, 2, 1, 4, 6)))
+        assertEquals(6, finder2(arrayOf(9, 8, 7, 6, 5, 4, 3, 2, 1), arrayOf(9, 8, 7, 5, 4, 3, 2, 1)))
+    }
+
+    @Test
+    fun testFinder3() {
+        assertEquals(5, finder3(arrayOf(5, 5, 7, 7), arrayOf(5, 7, 7)))
+        assertEquals(5, finder3(arrayOf(1, 2, 3, 4, 5, 6, 7), arrayOf(3, 7, 2, 1, 4, 6)))
+        assertEquals(6, finder3(arrayOf(9, 8, 7, 6, 5, 4, 3, 2, 1), arrayOf(9, 8, 7, 5, 4, 3, 2, 1)))
     }
 }
