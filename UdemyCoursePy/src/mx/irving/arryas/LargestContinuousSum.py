fe87b60098cefb170c7c619b90f2f7085178f377
Irving Lopez <ilopezp@gbm.com.mx>
2019-03-27 18:38:10 -0600

    [Python Course] Largest Continuous Sum - course solution

--- UdemyCoursePy/src/mx/irving/arryas/LargestContinuousSum.py
+++ UdemyCoursePy/src/mx/irving/arryas/LargestContinuousSum.py
@@ -111,7 +111,14 @@ def larges_cont_sum_short(array):
     return max_sum
 
 
-larges_cont_sum_short([1, 2, -1, 3, 4, 10, 10, -10, -1])
+def larges_cont_sum_course(array):
+    if len(array) == 0:
+        return 0
+    max_sum = current_sum = array[0]
+    for num in array[1:]:
+        current_sum = max(current_sum + num, num)
+        max_sum = max(max_sum, current_sum)
+    return max_sum
 
 
 class TestLargestContinuousSum(unittest.TestCase):
@@ -133,6 +140,15 @@ class TestLargestContinuousSum(unittest.TestCase):
         self.assertEqual(larges_cont_sum_short([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6)
         self.assertEqual(larges_cont_sum_short([1, -1, 1, -1, 10, -5, 1, -1, 3, -1]), 10)
         print('test_short pass')
+
+    def test_short(self):
+        self.assertEqual(larges_cont_sum_course([1, 2, -1, 3, 4, -1]), 9)
+        self.assertEqual(larges_cont_sum_course([1, 2, -1, 3, 4, 10, 10, -10, -1]), 29)
+        self.assertEqual(larges_cont_sum_course([-1, 1]), 1)
+        self.assertEqual(larges_cont_sum_course([-2, -3, 4, -1, -2, 1, 5, -3]), 7)
+        self.assertEqual(larges_cont_sum_course([-2, 1, -3, 4, -1, 2, 1, -5, 4]), 6)
+        self.assertEqual(larges_cont_sum_course([1, -1, 1, -1, 10, -5, 1, -1, 3, -1]), 10)
+        print('test_course pass')
 
 
 tests = TestLargestContinuousSum()
