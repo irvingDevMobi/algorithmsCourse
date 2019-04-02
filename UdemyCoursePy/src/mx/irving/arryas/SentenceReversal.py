2bd75bbee71b194a912f5e5d095c1629ae653ef4
Irving Lopez <ilopezp@gbm.com.mx>
2019-04-01 19:04:44 -0600

    [Python Course] Sentence Reversal - my solution, manually

--- UdemyCoursePy/src/mx/irving/arryas/SentenceReversal.py
+++ UdemyCoursePy/src/mx/irving/arryas/SentenceReversal.py
@@ -13,6 +13,25 @@ def reverse_sentence(sentence):
     return inverted
 
 
+def reverse_manually(sentence):
+    word = ""
+    clean_words = []
+    for ch in sentence:
+        if ch == " " and len(word) != 0:
+            clean_words.append(word)
+            word = ""
+        elif ch != " ":
+            word += ch
+    if len(word) > 0:
+        clean_words.append(word)
+    reversed_words = ""
+    for index in range(len(clean_words) - 1, -1, -1):
+        reversed_words += clean_words[index]
+        if index != 0:
+            reversed_words += ' '
+    return reversed_words
+
+
 class ReverseSentenceTest(unittest.TestCase):
 
     def test(self):
@@ -20,3 +39,9 @@ class ReverseSentenceTest(unittest.TestCase):
         self.assertEqual('after space', reverse_sentence('space after     '))
         self.assertEqual('you are how John Hello', reverse_sentence('   Hello John    how are you   '))
         self.assertEqual('1', reverse_sentence('1'))
+
+    def test_manually(self):
+        self.assertEqual('before space', reverse_manually('     space before'))
+        self.assertEqual('after space', reverse_manually('space after     '))
+        self.assertEqual('you are how John Hello', reverse_manually('   Hello John    how are you   '))
+        self.assertEqual('1', reverse_manually('1'))
