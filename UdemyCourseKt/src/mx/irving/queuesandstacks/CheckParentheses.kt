a75e0bd43eea4dcdd6f43f9d334e559bc5c53804
Irving Lop <nirving21@gmail.com>
2019-04-22 11:29:14 -0500

    [Kotlin Project] Stacks and Queues: Check Parentheses - my solution

---
+++ UdemyCourseKt/src/mx/irving/queuesandstacks/CheckParentheses.kt
@@ -0,0 +1,23 @@
+package mx.irving.queuesandstacks
+
+fun checkParentheses(input: String): Boolean {
+	val stack = mutableListOf<Char>()
+	for(c in input) {
+		when(c) {
+			'{', '(', '[' -> stack.add(c)
+			'}' -> {
+					val top = stack.removeAt(stack.size -1)
+					if (top != '{') return false
+				}
+			')' -> {
+					val top = stack.removeAt(stack.size -1)
+					if (top != '(') return false
+				}
+			']' -> {
+					val top = stack.removeAt(stack.size -1)
+					if (top != '(') return false
+				}
+		}
+	}
+	return true
+}
---
+++ UdemyCourseKt/src/test/stacksandqueues/CheckParenthesesTest.kt
@@ -0,0 +1,12 @@
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class CheckParenthesesTest {
+
+	@Test
+	fun test() {
+		assertEquals(false, checkParentheses("[](){([[[]]])}("))
+		assertEquals(true,checkParentheses("[{{{(())}}}]((()))"))
+		assertEquals(false, checkParentheses("[[[]])]"))
+	}
+}
