727d336dabbc985e9066d07b292d5850c78e5764
Irving Lopez <ilopezp@gbm.com.mx>
2019-04-24 18:11:21 -0500

    [Python Course] Stacks and Queues: Check Parentheses - Course Solution

--- UdemyCoursePy/src/mx/irving/stacksandqueuesandmore/ParenthesesCheck.py
+++ UdemyCoursePy/src/mx/irving/stacksandqueuesandmore/ParenthesesCheck.py
@@ -20,10 +20,34 @@ def balance_check(parentheses):
     return len(stack) == 0
 
 
+def balance_check_u(parentheses):
+    if len(parentheses) % 2 != 0:
+        return False
+    opening = set('([{')
+    matches = {('(', ')'), ('[', ']'), ('{', '}')}
+    stack = []
+    for char in parentheses:
+        if char in opening:
+            stack.append((char))
+        else:
+            if len(stack) == 0:
+                return False
+            last_open = stack.pop()
+            if (last_open, char) not in matches:
+                return False
+    return len(stack) == 0
+
+
 class ParenthesesCheckTest(unittest.TestCase):
 
     def test(self):
         self.assertEqual(balance_check('[](){([[[]]])}('), False)
         self.assertEqual(balance_check('[{{{(())}}}]((()))'), True)
         self.assertEqual(balance_check('[[[]])]'), False)
+        print('ALL TEST CASES PASSED')
+
+    def test_u(self):
+        self.assertEqual(balance_check_u('[](){([[[]]])}('), False)
+        self.assertEqual(balance_check_u('[{{{(())}}}]((()))'), True)
+        self.assertEqual(balance_check_u('[[[]])]'), False)
         print('ALL TEST CASES PASSED')
