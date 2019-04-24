aca2756d1b2be558c6751e4d4fd01bdb7127bc12
Irving Lopez <ilopezp@gbm.com.mx>
2019-04-24 18:30:46 -0500

    [Kotlin] Stacks and Queues: Check Parentheses - Course solution

--- UdemyCourseKt/src/main/kotlin/mx/irving/stacksandqueues/CheckParentheses.kt
+++ UdemyCourseKt/src/main/kotlin/mx/irving/stacksandqueues/CheckParentheses.kt
@@ -1,23 +1,41 @@
 package mx.irving.stacksandqueues
 
 fun checkParentheses(input: String): Boolean {
-	val stack = mutableListOf<Char>()
-	for(c in input) {
-		when(c) {
-			'{', '(', '[' -> stack.add(c)
-			'}' -> {
-					val top = stack.removeAt(stack.size -1)
-					if (top != '{') return false
-				}
-			')' -> {
-					val top = stack.removeAt(stack.size -1)
-					if (top != '(') return false
-				}
-			']' -> {
-					val top = stack.removeAt(stack.size -1)
-					if (top != '[') return false
-				}
-		}
-	}
-	return stack.isEmpty()
+    if (input.length % 2 != 0) return false
+    val stack = mutableListOf<Char>()
+    for (c in input) {
+        when (c) {
+            '{', '(', '[' -> stack.add(c)
+            '}' -> {
+                val top = stack.removeAt(stack.size - 1)
+                if (top != '{') return false
+            }
+            ')' -> {
+                val top = stack.removeAt(stack.size - 1)
+                if (top != '(') return false
+            }
+            ']' -> {
+                val top = stack.removeAt(stack.size - 1)
+                if (top != '[') return false
+            }
+        }
+    }
+    return stack.isEmpty()
+}
+
+fun checkParenthesesUde(input: String): Boolean {
+    if (input.length % 2 != 0) return false
+    val openings = setOf('(', '[', '{')
+    val matches = setOf(Pair('(', ')'), Pair('[', ']'), Pair('{', '}'))
+    val stack = mutableListOf<Char>()
+    for (c in input) {
+        if (c in openings)
+            stack.add(c)
+        else {
+            if (stack.size == 0) return false
+            val lastOpen = stack.removeAt(stack.size - 1)
+            if (Pair(lastOpen, c) !in matches) return false
+        }
+    }
+    return stack.isEmpty()
 }
--- UdemyCourseKt/src/test/kotlin/mx/irving/stacksandqueues/CheckParenthesesTest.kt
+++ UdemyCourseKt/src/test/kotlin/mx/irving/stacksandqueues/CheckParenthesesTest.kt
@@ -5,10 +5,21 @@ import org.junit.Test
 
 class CheckParenthesesTest {
 
-	@Test
-	fun test() {
-		assertEquals(false, checkParentheses("[](){([[[]]])}("))
-		assertEquals(true,checkParentheses("[{{{(())}}}]((()))"))
-		assertEquals(false, checkParentheses("[[[]])]"))
-	}
+    @Test
+    fun test() {
+        // 4 ms
+        assertEquals(false, checkParentheses("[](){([[[]]])}("))
+        assertEquals(true, checkParentheses("[{{{(())}}}]((()))"))
+        assertEquals(true, checkParentheses("[{{{(())}}}]((()))[{{{(())}}}]((()))[{{{(())}}}]((()))[{{{(())}}}]((()))"))
+        assertEquals(false, checkParentheses("[[[]])]"))
+    }
+
+    @Test
+    fun testUde() {
+        // 58 ms
+        assertEquals(false, checkParenthesesUde("[](){([[[]]])}("))
+        assertEquals(true, checkParenthesesUde("[{{{(())}}}]((()))"))
+        assertEquals(true, checkParenthesesUde("[{{{(())}}}]((()))[{{{(())}}}]((()))[{{{(())}}}]((()))[{{{(())}}}]((()))"))
+        assertEquals(false, checkParenthesesUde("[[[]])]"))
+    }
 }
