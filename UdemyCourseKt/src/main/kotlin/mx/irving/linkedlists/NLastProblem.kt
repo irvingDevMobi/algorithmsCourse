a0109507a89c62c7767a83578e773e6b5fd28121
Irving Lopez <ilopezp@gbm.com.mx>
2019-05-13 19:44:15 -0500

    [Kotlin] Linked List: Nth to Last - my solution

---
+++ UdemyCourseKt/src/main/kotlin/mx/irving/linkedlists/NLastProblem.kt
@@ -0,0 +1,17 @@
+package mx.irving.linkedlists
+
+fun nToLast(n: Int, head: Node): Node? {
+    if (n == 0) return null
+    var count = 1
+    var pivot: Node? = head
+    var searcher: Node? = head
+    while (count < n) {
+        pivot = pivot?.nextNode
+        count += 1
+    }
+    while (pivot?.nextNode != null) {
+        searcher = searcher?.nextNode
+        pivot = pivot.nextNode
+    }
+    return searcher
+}
---
+++ UdemyCourseKt/src/test/kotlin/mx/irving/linkedlists/NLastProblemKtTest.kt
@@ -0,0 +1,37 @@
+package mx.irving.linkedlists
+
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class NLastProblemKtTest {
+
+    val a = Node(1)
+    val b = Node(2)
+    val c = Node(3)
+    val d = Node(4)
+    val e = Node(5)
+
+    @Before
+    fun init() {
+        a.nextNode = b
+        b.nextNode = c
+        c.nextNode = d
+        d.nextNode = e
+    }
+
+    @Test
+    fun test_common() {
+        assertEquals(d, nToLast(2, a))
+    }
+
+    @Test
+    fun test_1toLast() {
+        assertEquals(e, nToLast(1, a))
+    }
+
+    @Test
+    fun test_allToLast() {
+        assertEquals(a, nToLast(5, a))
+    }
+}
