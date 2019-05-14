9586c804b9556157a7e5cd4088b9a0a6ff831dd7
Irving Lopez <ilopezp@gbm.com.mx>
2019-05-13 19:26:53 -0500

    [Python Course] Linked List: Nth to Last Node - my solution

---
+++ UdemyCoursePy/src/mx/irving/linkedlist/NLastProblem.py
@@ -0,0 +1,41 @@
+import unittest
+
+from mx.irving.linkedlist.Node import Node
+
+
+def n_to_last(n, head):
+    if n == 0:
+        return None
+    count = 1
+    pivot = head
+    searcher = head
+    while count < n:
+        pivot = pivot.next
+        count += 1
+    while pivot.next is not None:
+        searcher = searcher.next
+        pivot = pivot.next
+    return searcher
+
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+d = Node(4)
+e = Node(5)
+a.next = b
+b.next = c
+c.next = d
+d.next = e
+
+
+class NToLastTest(unittest.TestCase):
+
+    def test(self):
+        self.assertEqual(d, n_to_last(2, a))
+
+    def test_1toLast(self):
+        self.assertEqual(e, n_to_last(1, a))
+
+    def test_allToLast(self):
+        self.assertEqual(a, n_to_last(5, a))
