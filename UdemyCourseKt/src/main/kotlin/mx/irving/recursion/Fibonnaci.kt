54cc3c09c957d413f89e657cc3e82a0e9d37f39e
Irving Lopez <ilopezp@gbm.com.mx>
2019-08-20 10:28:57 -0500

    [Python Course Kt] Recursion: Fibonnaci Dynamic- my solution

--- UdemyCourseKt/src/main/kotlin/mx/irving/recursion/Fibonnaci.kt
+++ UdemyCourseKt/src/main/kotlin/mx/irving/recursion/Fibonnaci.kt
@@ -2,8 +2,39 @@ package mx.irving.recursion
 
 fun fibonnaci(num: Int): Long {
     //println("Fibonnaci of $num")
-    return if (num < 1) 0
-    else if (num == 1) 1
-    else fibonnaci(num - 1) + fibonnaci(num - 2)
+    return when {
+        num < 1 -> 0
+        num == 1 -> 1
+        else -> fibonnaci(num - 1) + fibonnaci(num - 2)
+    }
 }
 
+val cache = LongArray(30) { -1L }
+
+fun fibonnaciDynamic(num: Int): Long {
+    return when {
+        num < 0 -> 0
+        num < cache.size && cache[num] >= 0 -> cache[num]
+        num < 1 -> {
+            cache[0] = 0
+            cache[0]
+        }
+        num == 1 -> {
+            cache[1] = 1
+            cache[1]
+        }
+        else -> {
+            val oneBackNum = num - 1
+            val twoBackNum = num - 2
+            val fibonnaciOneBack = fibonnaciDynamic(oneBackNum)
+            val fibonnaciTwoBack = fibonnaciDynamic(twoBackNum)
+            if (twoBackNum < cache.size) {
+                cache[twoBackNum] = fibonnaciTwoBack
+                if (oneBackNum < cache.size) {
+                    cache[oneBackNum] = fibonnaciOneBack
+                }
+            }
+            fibonnaciOneBack + fibonnaciTwoBack
+        }
+    }
+}
--- UdemyCourseKt/src/test/kotlin/mx/irving/recursion/FibonnaciKtTest.kt
+++ UdemyCourseKt/src/test/kotlin/mx/irving/recursion/FibonnaciKtTest.kt
@@ -11,5 +11,15 @@ class FibonnaciKtTest {
         assertEquals(1, fibonnaci(1))
         assertEquals(28657, fibonnaci(23))
         assertEquals(1346269, fibonnaci(31))
+        assertEquals(165580141, fibonnaci(41))
+    }
+
+    @Test
+    fun fibonnaciDynamic_ok() {
+        assertEquals(55, fibonnaciDynamic(10))
+        assertEquals(1, fibonnaciDynamic(1))
+        assertEquals(28657, fibonnaciDynamic(23))
+        assertEquals(1346269, fibonnaciDynamic(31))
+        assertEquals(165580141, fibonnaciDynamic(41))
     }
 }
