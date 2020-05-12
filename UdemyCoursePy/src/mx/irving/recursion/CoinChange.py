1c3aa7a8f42e77bf81da8170c4f0f082653360f5
Irving Lop <ilopezp@gbm.com.mx>
2020-05-11 19:32:46 -0500

    [Udemy Py] Recursion: Coin Change - Course Solution

--- UdemyCoursePy/src/mx/irving/recursion/CoinChange.py
+++ UdemyCoursePy/src/mx/irving/recursion/CoinChange.py
@@ -25,6 +25,41 @@ def get_min_coins(target, coins):
         return sum_coins + get_min_coins(new_target, coins[:len(coins) - 1])
 
 
+# Dynamic course solution
+def rec_coin_dynam(target, coins, know_results):
+    """
+        INPUT: This function takes in a target amount and a list of possible coins to use.
+        It also takes a third parameter, known_results, indicating previously calculated results.
+        The known_results parameter should be started with [0] * (target+1)
+
+        OUTPUT: Minimum number of coins needed to make the target.
+    """
+
+    # Default output to target
+    min_coins = target
+
+    # Base Case
+    if target in coins:
+        know_results[target] = 1
+        return 1
+    elif know_results[target] > 0:
+        return know_results[target]
+    else:
+        # for every coin value thar is <= than target
+        for i in [c for c in coins if c <= target]:
+
+            # Recursive call, note how we include the know results!
+            num_coins = 1 + rec_coin_dynam(target - i, coins, know_results)
+
+            # Reset minimum if we have a new minimum
+            if num_coins < min_coins:
+                min_coins = num_coins
+
+                # Update the know result
+                know_results[target] = min_coins
+    return min_coins
+
+
 class GetMinCoinsTest(unittest.TestCase):
 
     def test(self):
@@ -32,3 +67,15 @@ class GetMinCoinsTest(unittest.TestCase):
         self.assertEqual(get_min_coins(45, coins), 3)
         self.assertEqual(get_min_coins(23, coins), 5)
         self.assertEqual(get_min_coins(74, coins), 8)
+
+    def test_udemy_sol(self):
+        coins = [1, 5, 10, 25]
+        target = 45
+        known_results = [0] * (target + 1)
+        self.assertEqual(rec_coin_dynam(target, coins, known_results), 3)
+        target = 23
+        known_results = [0] * (target + 1)
+        self.assertEqual(rec_coin_dynam(target, coins, known_results), 5)
+        target = 74
+        known_results = [0] * (target + 1)
+        self.assertEqual(rec_coin_dynam(target, coins, known_results), 8)
