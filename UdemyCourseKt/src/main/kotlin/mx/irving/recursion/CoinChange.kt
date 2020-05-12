9cb2630f5ff5fe01953b9c2c5ceee9a530e227e2
Irving Lop <ilopezp@gbm.com.mx>
2020-05-11 20:45:45 -0500

    [Udemy kt] Recursion: Coin Change - Course Solution

--- UdemyCourseKt/src/main/kotlin/mx/irving/recursion/CoinChange.kt
+++ UdemyCourseKt/src/main/kotlin/mx/irving/recursion/CoinChange.kt
@@ -27,3 +27,48 @@ fun getMinCoins(target: Int, coins: List<Int>): Int {
         coinsNumber + getMinCoins(newTarget, coins.subList(0, coins.size - 1))
     }
 }
+
+/**
+ * Udemy Course Solution
+ *
+ * INPUT: This funciton takes in a target amount and a list of possible coins to use.
+ * It also takes a third parameter, known_results, indicating previously calculated results.
+ * The known_results parameter shoud be started with [0] * (target+1)
+ * OUTPUT: Minimum number of coins needed to make the target.
+ *
+ */
+fun getMinCoinsDynamic(
+        target: Int, coins: List<Int>,
+        knowResults: MutableList<Int> = MutableList(target + 1) { 0 }
+): Int {
+    // Default output to target
+    var minCoins = target
+
+    when {
+        // Base Case
+        target in coins -> {
+            knowResults[target] = 1
+            return 1
+        }
+        // Return a known result if it happens to be greater than 1
+        knowResults[target] > 0 -> {
+            return knowResults[target]
+        }
+        else -> {
+            // for every coin value that is <= than target
+            for (i in coins.filter { it <= target }) {
+                //Recursive call, note how we include the known results
+                val numCoins = 1 + getMinCoinsDynamic(target - i, coins, knowResults)
+
+                // Reset minimum if we have a new minimum
+                if (numCoins < minCoins) {
+                    minCoins = numCoins
+                    // Reset the know result
+                    knowResults[target] = minCoins
+                }
+            }
+        }
+    }
+    return minCoins
+}
+
--- UdemyCourseKt/src/test/kotlin/mx/irving/recursion/CoinChangeKtTest.kt
+++ UdemyCourseKt/src/test/kotlin/mx/irving/recursion/CoinChangeKtTest.kt
@@ -12,4 +12,12 @@ class CoinChangeKtTest {
         assertEquals(5, getMinCoins(23, coins))
         assertEquals(8, getMinCoins(74, coins))
     }
+
+    @Test
+    fun getMinCoinsDynamic_ok() {
+        val coins = listOf(1, 5, 10, 25)
+        assertEquals(3, getMinCoinsDynamic(45, coins))
+        assertEquals(5, getMinCoinsDynamic(23, coins))
+        assertEquals(8, getMinCoinsDynamic(74, coins))
+    }
 }
